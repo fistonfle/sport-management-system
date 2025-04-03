2d4ff2ad99457d4ef4e2aa1b67ae0fe91b6c8dea
fistonfle <fistonfle04@gmail.com>
2025-04-03 20:22:53 +0200

    removed secrets

--- gym-management-backend/models/classModel.js
+++
@@ -1,6 +0,0 @@
-const { db } = require("../utils/firebaseAdmin");
-
-// Firestore reference to 'classes' collection
-const classesCollection = db.collection("classes");
-
-module.exports = { classesCollection };
--- gym-management-backend/models/clientModel.js
+++
@@ -1,6 +0,0 @@
-const { db } = require("../utils/firebaseAdmin");
-
-// Firestore reference to 'clients' collection
-const clientsCollection = db.collection("clients");
-
-module.exports = { clientsCollection };
