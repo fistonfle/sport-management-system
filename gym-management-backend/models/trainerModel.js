bd1e85b2becbbf23eda4bcab0669913d8b580ea7
fistonfle <fistonfle04@gmail.com>
2025-04-03 20:13:23 +0200

    removed screts

--- gym-management-backend/models/trainerModel.js
+++
@@ -1,6 +0,0 @@
-const { db } = require("../utils/firebaseAdmin");
-
-// Firestore reference to 'trainers' collection
-const trainersCollection = db.collection("trainers");
-
-module.exports = { trainersCollection };
--- gym-management-backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-
-const express = require("express");
-const { registerUser } = require("../controllers/authController");
-
-const router = express.Router();
-
-router.post("/signup", registerUser);
-
-module.exports = router;
